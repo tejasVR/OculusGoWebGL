cf059dd91e19414d3218fbad1acc6584d9c9e4f6
tejasVR <tejasshroff24@gmail.com>
2020-04-16 16:00:28 -0400

    Fire pass at moving a controller independant of camera

---
+++ Assets/App/Scripts/ControllerSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XPO
+{
+    public class ControllerSwitch : MonoBehaviour
+    {
+        [SerializeField] MouseLook cameraLook;
+        [SerializeField] MouseLook controllerLook;
+
+        private bool controllerOn;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                ToggleControllerLook(controllerOn);
+            }
+        }
+
+        private void ToggleControllerLook(bool _controllerOn)
+        {                        
+            controllerLook.enabled = !_controllerOn;
+            cameraLook.enabled = _controllerOn;
+            controllerOn = !_controllerOn;            
+        }
+    }
+
+}
---
+++ Assets/App/Scripts/MouseLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XPO
+{
+    public class MouseLock : MonoBehaviour
+    {      
+        void Start()
+        {
+            // Hide cursor while in-game
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }     
+    }
+}
+
+
